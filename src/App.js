7d98add7b7a34f6d7dbbabbecc63d688c35ae19c
Facundo Martin <facundomartin98@gmail.com>
2022-12-27 16:26:46 -0800

    set up useMode in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-  return <div className="app">App</div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
