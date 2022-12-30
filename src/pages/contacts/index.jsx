9649a971d6b63c8ed53b9e25ba0748790d20388b
Facundo Martin <facundomartin98@gmail.com>
2022-12-30 09:21:36 -0800

    added datagrid styles

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -54,7 +54,38 @@ const Contacts = () => {
 
   return (
     <PageWrapper pageTitle="Contacts" pageSubtitle="List of contacts">
-      <Box m="40px 0 0 0" height="75vh">
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
         <DataGrid
           rows={mockDataContacts}
           columns={columns}
