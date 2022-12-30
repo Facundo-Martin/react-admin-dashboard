e75922c18ce1ea7a0c4c86fc8cba652ea9b41dc0
Facundo Martin <facundomartin98@gmail.com>
2022-12-30 09:28:01 -0800

    added mui data grid to Invoices page

--- src/pages/invoices/index.jsx
+++ src/pages/invoices/index.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataInvoices } from "../../data/mockData";
+import PageWrapper from "../../components/PageWrapper";
 
 const Invoices = () => {
-  return <div>Invoices</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]}>
+          ${params.row.cost}
+        </Typography>
+      ),
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <PageWrapper pageTitle={"Invoices"} pageSubtitle="Invoice Balances">
+      <Box m="40px 0 0 0" height="75vh">
+        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+      </Box>
+    </PageWrapper>
+  );
 };
 
 export default Invoices;
