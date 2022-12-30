08f1f43d27a372e54ee9a1f39c2dc44226852b14
Facundo Martin <facundomartin98@gmail.com>
2022-12-30 09:21:24 -0800

    added contact page mui data grid

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataContacts } from "../../data/mockData";
+import { useTheme } from "@mui/material";
+import PageWrapper from "../../components/PageWrapper";
 
 const Contacts = () => {
-  return <div>Contacts</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.5 },
+    { field: "registrarId", headerName: "Registrar ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+    },
+    {
+      field: "zipCode",
+      headerName: "Zip Code",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <PageWrapper pageTitle="Contacts" pageSubtitle="List of contacts">
+      <Box m="40px 0 0 0" height="75vh">
+        <DataGrid
+          rows={mockDataContacts}
+          columns={columns}
+          components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
+    </PageWrapper>
+  );
 };
 
 export default Contacts;
