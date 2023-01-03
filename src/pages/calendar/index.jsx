c2ad159c889790c47171c6e9a5dfb463b83438ce
Facundo Martin <facundomartin98@gmail.com>
2023-01-03 14:50:21 -0800

    added Calendar grid layout

--- src/pages/calendar/index.jsx
+++ src/pages/calendar/index.jsx
@@ -13,6 +13,7 @@ import {
   useTheme,
 } from "@mui/material";
 import { tokens } from "../../theme";
+import PageWrapper from "../../components/PageWrapper";
 
 const Calendar = () => {
   const theme = useTheme();
@@ -35,7 +36,49 @@ const Calendar = () => {
     }
   };
 
-  return <div>Calendar</div>;
+  return (
+    <PageWrapper
+      pageTitle="Calendar"
+      pageSubtitle="Full Calendar Interactive Page"
+    >
+      <Box display="flex" justifyContent="space-between">
+        {/* CALENDAR SIDEBAR */}
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
+    </PageWrapper>
+  );
 };
 
 export default Calendar;
