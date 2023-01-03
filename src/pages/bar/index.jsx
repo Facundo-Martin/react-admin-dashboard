b10403054ac8e5b246c7e04b16b8a81a2e597b01
Facundo Martin <facundomartin98@gmail.com>
2023-01-03 15:17:20 -0800

    added Bar Chart page

--- src/pages/bar/index.jsx
+++ src/pages/bar/index.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Box } from "@mui/material";
+import BarChart from "../../components/BarChart";
+import PageWrapper from "../../components/PageWrapper";
 
 const Bar = () => {
-  return <div>Bar</div>;
+  return (
+    <PageWrapper pageTitle="Bar Chart" pageSubtitle="Simple Bar Chart">
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </PageWrapper>
+  );
 };
 
 export default Bar;
