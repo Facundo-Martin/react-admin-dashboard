0e7b0a02dc2051e10b60aafabc26365bc2f5a1f3
Facundo Martin <facundomartin98@gmail.com>
2023-01-03 15:00:04 -0800

    added full calendar

--- src/pages/calendar/index.jsx
+++ src/pages/calendar/index.jsx
@@ -37,6 +37,16 @@ const Calendar = () => {
     }
   };
 
+  const handleEventClick = (selected) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${selected.event.title}'`
+      )
+    ) {
+      selected.event.remove();
+    }
+  };
+
   return (
     <PageWrapper
       pageTitle="Calendar"
@@ -76,6 +86,43 @@ const Calendar = () => {
               </ListItem>
             ))}
           </List>
+        </Box>
+        {/* CALENDAR */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: "12315",
+                title: "All-day event",
+                date: "2022-09-14",
+              },
+              {
+                id: "5123",
+                title: "Timed event",
+                date: "2022-09-28",
+              },
+            ]}
+          />
         </Box>
       </Box>
     </PageWrapper>
